b5ef7d9a8bf91c2b34c8c32ed751ddd7c1c20e07
Giovanna-Manfredo <giovanna.candido.manfredo@gmail.com>
2022-10-17 19:45:40 -0300

    fix: tags
    remove migrations

--- database/migrations/1665293873044_tags_unique.ts
+++
@@ -1,9 +0,0 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
-
-export default class extends BaseSchema {
-  public up() {
-    this.schema.alterTable('tags', (table) => {
-      table.string('name').notNullable()
-    })
-  }
-}
