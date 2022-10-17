2ca97ec74e111a3998893f7f35cc720589c48431
Giovanna-Manfredo <giovanna.candido.manfredo@gmail.com>
2022-10-17 19:33:25 -0300

    feat: a

--- database/migrations/1665293873044_tags_unique.ts
+++ database/migrations/1665293873044_tags_unique.ts
@@ -3,7 +3,6 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   public up() {
     this.schema.alterTable('tags', (table) => {
-      table.dropColumn('name')
       table.string('name').notNullable()
     })
   }
